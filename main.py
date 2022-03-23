cfe9abe23fa02fe36e6383db00483af11124a5ac
MrDream92 <a.morozov.92@mail.ru>
2022-03-23 20:34:53 +0700

    3
    3

--- main.py
+++ main.py
@@ -1,26 +1,3 @@
-"""from aiogram import Bot, Dispatcher, executor, types
-from config import *
-
-bot = Bot(BOT_TOKEN)
-dp = Dispatcher(bot)
-
-@dp.message_handler(commands=['start', 'help'])
-async def welcome(message: types.Message):
-    await message.reply("Hello! Im Gunther Bot, Please follow my YT channel")
-
-@dp.message_handler(commands=['logo'])
-async def logo(message: types.Message):
-    await message.answer_photo('https://avatars.githubusercontent.com/u/62240649?v=4')
-
-@dp.message_handler()
-async def echo(message: types.Message):
-    text = f"Привет, ты написал: {message.text}"
-    await message.reply(text=text)
-
-
-if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)"""
-
 import logging
 import os
 from aiogram import Bot
@@ -28,12 +5,14 @@ from aiogram.dispatcher import Dispatcher
 from aiogram.utils.executor import start_webhook
 from aiogram import Bot, types
 
+BOT_TOKEN = "5182616067:AAEPUM-mkz1Ier0a4J7DyykHsSfeb1bTIMU"
+HEROKU_APP_NAME = "yarche-help"
 
-TOKEN = os.getenv('BOT_TOKEN')
+TOKEN = os.getenv(BOT_TOKEN)
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
-HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
+HEROKU_APP_NAME = os.getenv(HEROKU_APP_NAME)
 
 # webhook settings
 WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
@@ -58,23 +37,7 @@ async def echo(message: types.Message):
     await message.answer(message.text)
 
 
- if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
-    start_webhook(
-        dispatcher=dp,
-        webhook_path=WEBHOOK_PATH,
-        skip_updates=True,
-        on_startup=on_startup,
-        on_shutdown=on_shutdown,
-        host=WEBAPP_HOST,
-        port=WEBAPP_PORT,
-    )
-@dp.message_handler()
-async def echo(message: types.Message):
-    await message.answer(message.text)
-
-
- if __name__ == '__main__':
+if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     start_webhook(
         dispatcher=dp,
