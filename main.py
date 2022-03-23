6d68f3794bd8066f56bd27f28e45d75be220819c
MrDream92 <a.morozov.92@mail.ru>
2022-03-23 20:22:29 +0700

    3
    3

--- main.py
+++ main.py
@@ -22,23 +22,66 @@ if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)"""
 
 import logging
+import os
+from aiogram import Bot
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils.executor import start_webhook
+from aiogram import Bot, types
 
-from aiogram import Bot, Dispatcher, executor, types
 
-# Токен, выданный BotFather в телеграмме
-API_TOKEN = "5182616067:AAEPUM-mkz1Ier0a4J7DyykHsSfeb1bTIMU"
-
-# Configure logging
-logging.basicConfig(level=logging.INFO)
-
-# Initialize bot and dispatcher
-bot = Bot(token=API_TOKEN)
+TOKEN = os.getenv('BOT_TOKEN')
+bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
+
+HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
+
+# webhook settings
+WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
+WEBHOOK_PATH = f'/webhook/{TOKEN}'
+WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
+
+# webserver settings
+WEBAPP_HOST = '0.0.0.0'
+WEBAPP_PORT = os.getenv('PORT', default=8000)
+
+
+async def on_startup(dispatcher):
+    await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
+
+
+async def on_shutdown(dispatcher):
+    await bot.delete_webhook()
+
 
 @dp.message_handler()
 async def echo(message: types.Message):
     await message.answer(message.text)
 
 
-if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+ if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        skip_updates=True,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT,
+    )
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer(message.text)
+
+
+ if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        skip_updates=True,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT,
+    )
