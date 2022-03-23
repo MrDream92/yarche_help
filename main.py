97abdd97dcde0ff19635017c26f4127f8ff95e38
MrDream92 <a.morozov.92@mail.ru>
2022-03-23 20:11:21 +0700

    2
    2

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from aiogram import Bot, Dispatcher, executor, types
+"""from aiogram import Bot, Dispatcher, executor, types
 from config import *
 
 bot = Bot(BOT_TOKEN)
@@ -16,6 +16,28 @@ async def logo(message: types.Message):
 async def echo(message: types.Message):
     text = f"Привет, ты написал: {message.text}"
     await message.reply(text=text)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)"""
+
+import logging
+
+from aiogram import Bot, Dispatcher, executor, types
+
+# Токен, выданный BotFather в телеграмме
+API_TOKEN = "5182616067:AAEPUM-mkz1Ier0a4J7DyykHsSfeb1bTIMU"
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer(message.text)
 
 
 if __name__ == '__main__':
