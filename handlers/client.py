10975fda3cc93ffc6ca65d5ecf1ce3d5caa4efe2
MrDream92 <a.morozov.92@mail.ru>
2022-04-03 20:28:57 +0700

    5
    5

--- handlers/client.py
+++ handlers/client.py
@@ -15,12 +15,36 @@ class FSM_user(StatesGroup):#Клас необходим для перехода
     mag_user = State()
 
 
-async def registration_start(message: types.Message):
-    await message.answer("Введите ваш номер телефона:")
-    await FSM_user.number_user.set()
+class reg(StatesGroup):
+    name = State()
+    fname = State()
+    age = State()
 
 
+#@dp.message_handler(commands="reg", state="*")
+async def name_step(message: types.Message, state: FSMContext):
+    await message.answer(text='Напиши имя ')
+    await reg.name.set()
 
+
+#@dp.message_handler(state=reg.name, content_types=types.ContentTypes.TEXT)
+async def fname_step(message: types.Message, state: FSMContext):
+    if any(map(str.isdigit, message.text)):
+        await message.reply("Пожалуйста напишите свое имя")
+        return
+    await state.update_data(name_user=message.text.title())
+    await message.answer(text='Напиши фамилию ')
+    await reg.fname.set()
+
+
+#@dp.message_handler(state=reg.fname, content_types=types.ContentTypes.TEXT)
+async def age_step(message: types.Message, state: FSMContext):
+    if any(map(str.isdigit, message.text)):
+        await message.reply("Пожалуйста напишите свою фамилию")
+        return
+    await message.answer(text='Напиши возраст ')
+    await state.update_data(fname_user=message.text.title())
+    await reg.age.set()
 
 
 async def echo(message: types.Message):
@@ -28,7 +52,11 @@ async def echo(message: types.Message):
 
 
 def register_handlers_client(dp:Dispatcher):
-    dp.register_message_handler(registration_start, commands='start', state="*")
+    #dp.register_message_handler(registration_start, commands='start', state="*")
+    dp.register_message_handler(name_step, commands="reg", state="*")
+    dp.register_message_handler(fname_step, state=reg.name, content_types=types.ContentTypes.TEXT)
+    dp.register_message_handler(age_step, state=reg.fname, content_types=types.ContentTypes.TEXT)
+
     dp.register_message_handler(echo)
 
 
