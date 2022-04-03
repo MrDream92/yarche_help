4d14fd38766938e373e58669cf895a42d1868d65
MrDream92 <a.morozov.92@mail.ru>
2022-04-03 20:10:34 +0700

    3
    3

---
+++ handlers/client — копия.py
@@ -0,0 +1,64 @@
+from aiogram import types, Dispatcher
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
+from create_bot import bot, dp
+
+button_registration = KeyboardButton("/Определить магазин")
+button_case_registration = ReplyKeyboardMarkup(resize_keyboard=True).add(button_registration)
+
+
+
+class FSMAdmin(StatesGroup):#Клас необходим для перехода между состояниями
+    mag = State()
+    last_number = State()
+
+
+"""async def start_user(message : types.Message):
+    await bot.send_message(message.from_user.id, )"""
+
+
+
+async def command_registration_user(message : types.Message):
+    await bot.send_message(message.from_user.id, "ратата " , reply_markup=button_case_registration)
+    #await message.delete()
+
+
+async def start_registration(message: types.Message):
+    await FSMAdmin.mag.set()
+    await message.reply("Введите номер магазина в формате Т001, Н002, М003...")
+
+
+async def load_mag(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['mag'] = message.text
+    await FSMAdmin.next()
+    await message.reply("Теперь введите 4 последние цифры вашего телефона")
+
+
+async def load_last_number(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['last_number'] = float(message.text)
+
+    async with state.proxy() as data:
+       await message.reply(str(data))#а вот и данные что мы навводили
+
+    #Тут запрос к БД
+
+    await state.finish()
+
+
+
+async def echo(message: types.Message):
+    await message.answer(message.text)
+
+
+def register_handlers_client(dp:Dispatcher):
+    #dp.register_message_handler(start_user, commands=['start'])
+    #dp.register_message_handler(command_registration_user, commands=['registration'])
+    #dp.register_message_handler(start_registration, commands=['Определить магазин'], state=None)
+    dp.register_message_handler(load_mag, state=FSMAdmin.mag)
+    dp.register_message_handler(load_last_number, state=FSMAdmin.last_number)
+    dp.register_message_handler(echo)
+
+
--- handlers/client.py
+++ handlers/client.py
@@ -7,45 +7,19 @@ from create_bot import bot, dp
 button_registration = KeyboardButton("/Определить магазин")
 button_case_registration = ReplyKeyboardMarkup(resize_keyboard=True).add(button_registration)
 
+mags = ["М001", "М002", "М003"]
 
 
-class FSMAdmin(StatesGroup):#Клас необходим для перехода между состояниями
-    mag = State()
-    last_number = State()
+class FSM_user(StatesGroup):#Клас необходим для перехода между состояниями
+    number_user = State()
+    mag_user = State()
 
 
-"""async def start_user(message : types.Message):
-    await bot.send_message(message.from_user.id, )"""
+async def registration_start(message: types.Message):
+    await message.answer("Введите ваш номер телефона:")
+    await FSM_user.number_user.set()
 
 
-
-async def command_registration_user(message : types.Message):
-    await bot.send_message(message.from_user.id, "ратата " , reply_markup=button_case_registration)
-    #await message.delete()
-
-
-async def start_registration(message: types.Message):
-    await FSMAdmin.mag.set()
-    await message.reply("Введите номер магазина в формате Т001, Н002, М003...")
-
-
-async def load_mag(message: types.Message, state: FSMContext):
-    async with state.proxy() as data:
-        data['mag'] = message.text
-    await FSMAdmin.next()
-    await message.reply("Теперь введите 4 последние цифры вашего телефона")
-
-
-async def load_last_number(message: types.Message, state: FSMContext):
-    async with state.proxy() as data:
-        data['last_number'] = float(message.text)
-
-    async with state.proxy() as data:
-       await message.reply(str(data))#а вот и данные что мы навводили
-
-    #Тут запрос к БД
-
-    await state.finish()
 
 
 
@@ -54,11 +28,7 @@ async def echo(message: types.Message):
 
 
 def register_handlers_client(dp:Dispatcher):
-    #dp.register_message_handler(start_user, commands=['start'])
-    dp.register_message_handler(command_registration_user, commands=['registration'])
-    dp.register_message_handler(start_registration, commands=['Определить магазин'], state=None)
-    dp.register_message_handler(load_mag, state=FSMAdmin.mag)
-    dp.register_message_handler(load_last_number, state=FSMAdmin.last_number)
     dp.register_message_handler(echo)
+    dp.register_message_handler(registration_start, commands='start', state="*")
 
 
