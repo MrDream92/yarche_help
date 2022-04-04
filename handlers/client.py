9fede527f9e46e7b2c4740bd4cc86e24ac4b960d
MrDream92 <a.morozov.92@mail.ru>
2022-04-04 22:55:28 +0700

    1
    1

--- handlers/client.py
+++ handlers/client.py
@@ -32,7 +32,7 @@ async def set_mag_number(message: types.Message, state: FSMContext):
 
     print(type(message.text))
     print(message.text)
-    db_object.execute(f'SELECT * FROM users WHERE user_number = {message.text}')
+    db_object.execute(f'SELECT * FROM users WHERE user_number = {str(message.text)}')
     result = db_object.fetchall()
     if not result:
         await message.answer(text='По данному номеру нет зарегистрированных магазинов... Обратитесь к администратору')
